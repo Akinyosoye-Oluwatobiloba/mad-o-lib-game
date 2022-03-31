648ee1f2bf5664155347e20e2beafbf99b4fab4c
The Annointed <96727089+Akinyosoye-Oluwatobiloba@users.noreply.github.com>
2022-03-31 17:35:32 +0100

    Add files via upload

---
+++ madorib.py
@@ -0,0 +1,12 @@
+# MADOLIB GAME
+name = input("Enter your name: ")
+age =  input("How old are you? :  ")
+subject = input("what\'s  your best subject? :") 
+food = input("what\'s  your best dish: ")
+church = input("what denomination do you attend: ") 
+
+print("My name is " + name)
+print("I\'m  " + age,"years old")
+print("My best subject is "+ subject )
+print("My best dish is "+food)
+print("I attend  "  + church)
